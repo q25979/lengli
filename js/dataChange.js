aa21307a3f78e2df85a3ca014c482c36bede189e
q25979 <277161542@qq.com>
2017-06-12 21:20:42 +0800

    前端布局逻辑完成

--- js/dataChange.js
+++ js/dataChange.js
@@ -15,10 +15,13 @@ $(function() {
 				content: '服务'
 			}, {
 				aHref: '#gallery',
-				content: '产品'
+				content: '我们的产品'
+			}, {
+				aHref: '#features',
+				content: '农产品作用'
 			}, {
 				aHref: '#team',
-				content: '我们团队'
+				content: '我们的团队'
 			}, {
 				aHref: '#contact',
 				content: '联系我们'
@@ -99,7 +102,148 @@ $(function() {
 	new Vue({
 		el: "#gallery",
 		data: {
-			title: "我们的产品"
+			title: "我们的产品",
+			img1: "images/pi.jpg",
+			img2: "images/pi2.jpg",
+			img3: "images/pi1.jpg",
+			img4: "images/pi4.jpg",
+			img5: "images/pi5.jpg",
+			img6: "images/pi3.jpg",
+			img7: "images/pi6.jpg",
+			img8: "images/pi7.jpg",
+		}
+	})
+
+	// 农产品的作用
+	new Vue({
+		el: "#features",
+		data: {
+			title: "农产品的作用",
+			description: "农产品作用的描述，这是一个关于农产品作用的描述...",
+			bigLi: [{
+				src: "images/fe.jpg",
+				description: "这是第一个描述",
+				smallLi: [{
+					content: "这是农产品的作用1"
+				}, {
+					content: "这是农产品的作用2"
+				}, {
+					content: "这是农产品的作用3"
+				}, {
+					content: "这是农产品的作用4"
+				}]
+			}, {
+				src: "images/fe1.jpg",
+				description: "这是第二个描述",
+				smallLi: [{
+					content: "这是农产品的作用1"
+				}, {
+					content: "这是农产品的作用2"
+				}, {
+					content: "这是农产品的作用3"
+				}, {
+					content: "这是农产品的作用4"
+				}]
+			}, {
+				src: "images/fe2.jpg",
+				description: "这是第三个描述",
+				smallLi: [{
+					content: "这是农产品的作用1"
+				}, {
+					content: "这是农产品的作用2"
+				}, {
+					content: "这是农产品的作用3"
+				}, {
+					content: "这是农产品的作用4"
+				}]
+			}, {
+				src: "images/fe3.jpg",
+				description: "这是第四个描述",
+				smallLi: [{
+					content: "这是农产品的作用1"
+				}, {
+					content: "这是农产品的作用2"
+				}, {
+					content: "这是农产品的作用3"
+				}, {
+					content: "这是农产品的作用4"
+				}]
+			}, ]
+		}
+	})
+
+	// 我们的团队
+	new Vue({
+		el: "#team",
+		data: {
+			title: "我们的团队",
+			list: [{
+				src: "url(images/te.jpg)",
+				name: "这是名字",
+				description: "相关的描述"
+			}, {
+				src: "url(images/te1.jpg)",
+				name: "这是名字",
+				description: "相关的描述"
+			}, {
+				src: "url(images/te2.jpg)",
+				name: "这是名字",
+				description: "相关的描述"
+			}, {
+				src: "url(images/te3.jpg)",
+				name: "这是名字",
+				description: "相关的描述"
+			}]
+		}
+	})
+
+	// 联系我们
+	new Vue({
+		el: "#contact",
+		data: {
+			title: "联系我们",
+			description: '这是联系我们的描述，描述内容自选',
+			content: "这是填内容的，想好什么内容就填",
+			list: [{
+				title: "我们的地址1",
+				description: "我们地址描述",
+				content: "我们地址的内容"
+			}, {
+				title: "我们的地址2",
+				description: "我们地址描述",
+				content: "我们地址的内容"
+			}],
+			footNavLi: [{
+				aHref: 'index.html',
+				content: '首页'
+			}, {
+				aHref: '#about',
+				content: '关于'
+			}, {
+				aHref: '#services',
+				content: '服务'
+			}, {
+				aHref: '#gallery',
+				content: '我们的产品'
+			}, {
+				aHref: '#features',
+				content: '农产品作用'
+			}, {
+				aHref: '#team',
+				content: '我们的团队'
+			}, {
+				aHref: '#contact',
+				content: '联系我们'
+			}, ],
+			footFriendLi: [{
+				classIcon: ''
+			}, {
+				classIcon: 'twitter'
+			}, {
+				classIcon: 'linked'
+			}, {
+				classIcon: 'dribble'
+			}]
 		}
 	})
 });
